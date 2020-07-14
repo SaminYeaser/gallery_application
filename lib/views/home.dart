00391ecd1326caa6121272159115e61bf62005ce
Samin <saminyeaser1@gmail.com>
2020-07-14 21:36:01 +0600

    category scrollable build

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,13 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:gallery/data/data.dart';
+import 'package:gallery/model/category_model.dart';
 import 'package:gallery/widgets/appBar.dart';
 
 class Home extends StatefulWidget {
+
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+
+  List<CategoryModel> categories = new List();
+  @override
+  void initState() {
+    categories = getCategories();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,9 +52,67 @@ class _HomeState extends State<Home> {
                   Icon(Icons.search)
                 ],
               ),
+            ),
+//            SizedBox(
+//              height: 2,
+//            ),
+            Container(
+              height: 60,
+              child: ListView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                shrinkWrap: true,
+              itemCount: categories.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index){
+                return CategoriTile(
+                      title: categories[index].categoryName,
+                      imgURL: categories[index].imgURL,
+                );
+                  }
+              ),
             )
           ],
         ),
+      ),
+    );
+  }
+}
+class CategoriTile extends StatelessWidget {
+  final String imgURL;
+  final String title;
+
+  const CategoriTile({Key key, @required this.imgURL,@required this.title}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 6),
+      child: Stack(
+        children: <Widget>[
+          ClipRRect(
+              child: Image.network(
+                imgURL,
+                height: 60,
+                width: 100,
+                fit: BoxFit.cover,
+              ),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.black26,
+            ),
+
+            height: 60, width: 100,
+            alignment: Alignment.center,
+            child: Text(
+              title,
+              style: TextStyle(
+                color: Colors.white
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
