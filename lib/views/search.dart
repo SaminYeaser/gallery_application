a487a332a1dcf249d6a18e64cf605635ede1bfd6
Samin <saminyeaser1@gmail.com>
2020-07-16 02:39:44 +0600

    copy pasting

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -1,19 +1,49 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:gallery/data/apikey.dart';
+import 'package:gallery/model/wallpaperModel.dart';
 import 'package:gallery/widgets/appBar.dart';
+import 'package:http/http.dart' as http;
 
 class Search extends StatefulWidget {
- final String searchQuery;
+  final String searchQuery;
+
   Search({this.searchQuery});
+
   @override
   _SearchState createState() => _SearchState();
 }
 
 class _SearchState extends State<Search> {
+  List<WallpaperModel> wallpaper = new List();
+  TextEditingController searchController = new TextEditingController();
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
   }
+
+  getSearchedWallpaper(String query) async{
+    var response = await http.get("https://api.pexels.com/v1/search?query=$query&per_page=100", headers: {
+      'Authorization': apiKey
+    });
+//    print(response.body.toString());
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+    jsonData['photos'].forEach((element){
+      print(element);
+      WallpaperModel wallpaperModel = new WallpaperModel();
+      wallpaperModel = WallpaperModel.fromMap(element);
+
+      wallpaper.add(wallpaperModel);
+    });
+    setState(() {
+
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +55,18 @@ class _SearchState extends State<Search> {
       body: Container(
         child: Column(
           children: <Widget>[
+            Expanded(
+              child: TextField(
+                controller: searchController,
+                decoration: InputDecoration(
+                    hintText: 'Search Wallpaper', border: InputBorder.none),
+              ),
+            ),
+            InkWell(
+                onTap: () {
 
+                },
+                child: Container(child: Icon(Icons.search)))
           ],
         ),
       ),
