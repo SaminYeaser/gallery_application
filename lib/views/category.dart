6934cac3ad48b3d3fa755794e02713c8babdd1a8
Samin <saminyeaser1@gmail.com>
2020-07-16 14:10:28 +0600

    category created

---
+++ lib/views/category.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class Category extends StatefulWidget {
+  @override
+  _CategoryState createState() => _CategoryState();
+}
+
+class _CategoryState extends State<Category> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
